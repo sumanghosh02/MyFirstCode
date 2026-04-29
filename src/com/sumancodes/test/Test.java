ab15e05833c21c89971cb15febee30faee09170e
sumanghosh02 <work.sumanghosh1@gmail.com>
2026-04-29 23:21:10 +0530

    init commit again.

--- src/com/sumancodes/test/Test.java
+++ src/com/sumancodes/test/Test.java
@@ -3,6 +3,7 @@ package com.sumancodes.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World  2");
 
     }
 }
